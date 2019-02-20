c225f88f64171d9486dce9f213be0a51f4a9a766
zaapen <zaapen@gmail.com>
2019-02-20 01:00:08 -0800

    added H1 and searchBox

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import Card from './Card';  // this is default, so it doesn't need {}
-import CardList from './CardList';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots';  // this is not default so it needs {}
 
-ReactDOM.render(
-  <CardList robots={robots} />
-  , document.getElementById('root'));  // greetings is a property that could be access in Hello.js
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
